fb0b1ca4e0d577a829c12a9354ed42e681b33e3c
NATRAJ-08 <146833134+NATRAJ-08@users.noreply.github.com>
2024-02-29 10:22:32 +0000

    update

--- database.py
+++ database.py
@@ -2,28 +2,51 @@ import pymysql
 import os
 
 my_secret = os.environ['PASSWORD']
+
 def load_jobs_from_db():
-  connection = pymysql.connect(
-      charset="utf8mb4",
-      connect_timeout=10,
-      cursorclass=pymysql.cursors.DictCursor,
-      db="defaultdb",
-      host="haribakshcareers-natraj-careers.a.aivencloud.com",
-      password=my_secret,
-      
-    
-      read_timeout=10,
-      port=28698,
-      user="avnadmin",
-      write_timeout=10,
-  )
+    connection = pymysql.connect(
+        charset="utf8mb4",
+        connect_timeout=10,
+        cursorclass=pymysql.cursors.DictCursor,
+        db="defaultdb",
+        host="haribakshcareers-natraj-careers.a.aivencloud.com",
+        password=my_secret,
+        read_timeout=10,
+        port=28698,
+        user="avnadmin",
+        write_timeout=10,
+    )
 
-  with connection.cursor() as cursor:
-      cursor.execute("SELECT * FROM jobs")
-      job = cursor.fetchall()
-      jobs = [dict(row) for row in job]
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM jobs")
+        job = cursor.fetchall()
+        jobs = [dict(row) for row in job]
 
-  connection.close()
+    connection.close()
 
-  return jobs
-  
+    return jobs
+
+def load_job_from_db(id):
+    connection = pymysql.connect(
+        charset="utf8mb4",
+        connect_timeout=10,
+        cursorclass=pymysql.cursors.DictCursor,
+        db="defaultdb",
+        host="haribakshcareers-natraj-careers.a.aivencloud.com",
+        password=my_secret,
+        read_timeout=10,
+        port=28698,
+        user="avnadmin",
+        write_timeout=10,
+    )
+
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM jobs WHERE id = %s", (id,))
+        job = cursor.fetchone()
+
+    connection.close()
+
+    if job:
+        return dict(job)
+    else:
+        return None
