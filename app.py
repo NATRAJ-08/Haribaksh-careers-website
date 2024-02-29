3bcc1517a5aba61d2f31599b53b7c0d0206c9f68
NATRAJ-08 <146833134+NATRAJ-08@users.noreply.github.com>
2024-02-29 08:28:20 +0000

    made some changes

--- app.py
+++ app.py
@@ -1,32 +1,7 @@
 from flask import Flask, render_template, jsonify
-import pymysql
+from database import load_jobs_from_db
 
 app = Flask(__name__)
-
-def load_jobs_from_db():
-    connection = pymysql.connect(
-        charset="utf8mb4",
-        connect_timeout=10,
-        cursorclass=pymysql.cursors.DictCursor,
-        db="defaultdb",
-        host="haribakshcareers-natraj-careers.a.aivencloud.com",
-        password="AVNS_1yapi6ol-2w_L7WvLpD",
-        read_timeout=10,
-        port=28698,
-        user="avnadmin",
-        write_timeout=10,
-    )
-
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM jobs")
-        job = cursor.fetchall()
-
-        # Convert the result to a list of dictionaries
-        jobs = [dict(row) for row in job]
-
-    connection.close()
-
-    return jobs
 
 @app.route('/')
 def hello_world():
--- database.py
+++ database.py
@@ -1,24 +1,27 @@
 import pymysql
 
 # Connect to the database using PyMySQL
-connection = pymysql.connect(
-    charset="utf8mb4",
-    connect_timeout=10,
-    cursorclass=pymysql.cursors.DictCursor,
-    db="defaultdb",
-    host="haribakshcareers-natraj-careers.a.aivencloud.com",
-    password="AVNS_1yapi6ol-2w_L7WvLpD",
-    read_timeout=10,
-    port=28698,
-    user="avnadmin",
-    write_timeout=10,
-)
+def load_jobs_from_db():
+  connection = pymysql.connect(
+      charset="utf8mb4",
+      connect_timeout=10,
+      cursorclass=pymysql.cursors.DictCursor,
+      db="defaultdb",
+      host="haribakshcareers-natraj-careers.a.aivencloud.com",
+      password="AVNS_1yapi6ol-2w_L7WvLpD",
+      read_timeout=10,
+      port=28698,
+      user="avnadmin",
+      write_timeout=10,
+  )
 
-with connection.cursor() as cursor:
-    cursor.execute("SELECT * FROM jobs")
-    result_pymysql = cursor.fetchall()
+  with connection.cursor() as cursor:
+      cursor.execute("SELECT * FROM jobs")
+      job = cursor.fetchall()
 
-    print(result_pymysql)  
-    for row in result_pymysql:
-        print(dict(row))
-connection.close()
+      # Convert the result to a list of dictionaries
+      jobs = [dict(row) for row in job]
+
+  connection.close()
+
+  return jobs
